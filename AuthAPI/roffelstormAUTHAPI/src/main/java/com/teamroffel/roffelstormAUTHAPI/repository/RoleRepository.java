05a1590d86841642afcfe89b860df1fdba4185e2
Erik Blixt Dackhammar <blixtdackhammar@gmail.com>
2020-11-20 13:27:43 +0100

    implemented repos

---
+++ AuthAPI/roffelstormAUTHAPI/src/main/java/com/teamroffel/roffelstormAUTHAPI/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.teamroffel.roffelstormAUTHAPI.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.teamroffel.roffelstormAUTHAPI.models.ERole;
+import com.teamroffel.roffelstormAUTHAPI.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
