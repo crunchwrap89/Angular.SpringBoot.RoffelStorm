2a2cf4623a15bc1bed8d81a701570ede02356b53
crunchwrap89 <marcus89n@gmail.com>
2020-12-11 10:14:35 +0100

    added code to delete account in frontend

--- FrontEnd/RoffelStormUI/src/app/profile/profile.component.ts
+++ FrontEnd/RoffelStormUI/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../_services/token-storage.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -7,13 +8,28 @@ import { TokenStorageService } from '../_services/token-storage.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
   currentUser: any;
+  status: any;
+  errorMessage: any;
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService,
+              private http: HttpClient) { }
+
 
   ngOnInit(): void {
     this.currentUser = this.token.getUser();
   }
 
+  deleteAccount(){
+  this.http.delete('http://localhost:8080/id')
+  .subscribe({
+      next: data => {
+          this.status = 'Delete successful';
+      },
+      error: error => {
+          this.errorMessage = error.message;
+          console.error('There was an error!', error);
+      }
+  });
+}
 }
