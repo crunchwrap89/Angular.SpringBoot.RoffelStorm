a38678b85ea856d5db52ced3126d9952ad56d2cd
AndersStationärDator <anders.maurits@hotmail.com>
2020-12-16 11:31:46 +0100

    added date

--- UserPostAPI/src/main/java/com/teamroffel/userpost/roffelstorm/model/UserPost.java
+++ UserPostAPI/src/main/java/com/teamroffel/userpost/roffelstorm/model/UserPost.java
@@ -38,18 +38,17 @@ public class UserPost {
     public UserPost() {
     }
 
-    public UserPost(Long id,  String text, int userId) {
-
-        this.id = id;
-        this.text = text;
-        this.userId = userId;
-        
-    }
+//    public UserPost(Long id,  String text, int userId) {
+//
+//        this.id = id;
+//        this.text = text;
+//        this.userId = userId;
+//        
+//    }
     
     
 
 	public UserPost(Long id, LocalDateTime localDateTime, int userId, String username, String text, int upvotes) {
-		super();
 		this.id = id;
 		this.localDateTime = localDateTime;
 		this.userId = userId;
@@ -58,54 +57,104 @@ public class UserPost {
 		this.upvotes = upvotes;
 	}
 
+	
+
 	public Long getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public String getText() {
-        return text;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setText(String text) {
-        this.text = text;
-    }
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
 
-    public int getUserId() {
-        return userId;
-    }
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+	public int getUserId() {
+		return userId;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 79 * hash + Objects.hashCode(this.id);
-        hash = 79 * hash + Objects.hashCode(this.text);
-        hash = 79 * hash + this.userId;
-        return hash;
-    }
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final UserPost other = (UserPost) obj;
-        if (this.userId != other.userId) {
-            return false;
-        }
-        if (!Objects.equals(this.text, other.text)) {
-            return false;
-        }
-        return Objects.equals(this.id, other.id);
-    }
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getUpvotes() {
+		return upvotes;
+	}
+
+	public void setUpvotes(int upvotes) {
+		this.upvotes = upvotes;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((localDateTime == null) ? 0 : localDateTime.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		result = prime * result + upvotes;
+		result = prime * result + userId;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserPost other = (UserPost) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (localDateTime == null) {
+			if (other.localDateTime != null)
+				return false;
+		} else if (!localDateTime.equals(other.localDateTime))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		if (upvotes != other.upvotes)
+			return false;
+		if (userId != other.userId)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
 
 }
