fdd3377bd00f2e7f5cd98da4a4d3a2ec3307d2f8
Erik Blixt Dackhammar <blixtdackhammar@gmail.com>
2021-01-14 14:26:32 +0100

    fixed API for handling Imageupload and saving to disk

--- FileAPI/FileUploader/src/main/java/com/bezkoder/spring/files/upload/db/message/ResponseFile.java
+++ FileAPI/FileUploader/src/main/java/com/bezkoder/spring/files/upload/db/message/ResponseFile.java
@@ -1,48 +1,24 @@
 package com.bezkoder.spring.files.upload.db.message;
 
 
+
 public class ResponseFile {
   private String name;
-  private String url;
-  private String type;
-  private long size;
-
-  public ResponseFile(String name, String url, String type, long size) {
+  private String path;
+  public ResponseFile(String name, String url) {
     this.name = name;
-    this.url = url;
-    this.type = type;
-    this.size = size;
+    this.path = url;
   }
-
   public String getName() {
     return name;
   }
-
   public void setName(String name) {
     this.name = name;
   }
-
-  public String getUrl() {
-    return url;
+  public String getPath() {
+    return path;
   }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public long getSize() {
-    return size;
-  }
-
-  public void setSize(long size) {
-    this.size = size;
+  public void setPath(String url) {
+    this.path = path;
   }
 }
