d3ebb8f1673795ad81e566d050a510835870c234
crunchwrap89 <marcus89n@gmail.com>
2021-01-18 16:09:21 +0100

    added code for userlist

---
+++ FrontEnd/RoffelStormUI/src/app/models/profilepost.ts
@@ -0,0 +1,10 @@
+export class ProfilePost {
+    id: number
+    authorId: number
+    date: any
+    text: string
+    recieverId: number
+    authorName: string
+    recieverName: string
+    upvotes: number
+  }
