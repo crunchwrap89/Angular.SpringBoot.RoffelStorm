6e2e9b3fbe5190e567512eb2442cefed04108e5b
Erik Blixt Dackhammar <blixtdackhammar@gmail.com>
2021-01-18 14:25:06 +0100

    Images are now connected to logged in user

--- FileAPI/FileUploader/src/main/java/com/bezkoder/spring/files/upload/db/model/FileDB.java
+++ FileAPI/FileUploader/src/main/java/com/bezkoder/spring/files/upload/db/model/FileDB.java
@@ -19,12 +19,17 @@ public class FileDB {
   private String id;
   private String name;
 
+
+
+  private int userId;
+
   private String path;
   public FileDB() {
   }
-  public FileDB(String name, String path) {
+  public FileDB(String name, String path, int userId) {
     this.name = name;
     this.path = path;
+    this.userId = userId;
   }
   public String getId() {
     return id;
@@ -38,6 +43,9 @@ public class FileDB {
   public String getPath() {
     return path;
   }
+  public int getUserId() { return userId; }
+
+  public void setUserId(int userId) { this.userId = userId; }
 
   public void setPath(String path) {
     this.path = path;
