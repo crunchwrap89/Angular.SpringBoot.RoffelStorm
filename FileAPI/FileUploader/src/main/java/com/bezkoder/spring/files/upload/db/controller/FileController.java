ac8f03ad0f5a94cb866ec2dd40a3a9d1d293f2be
Erik Blixt Dackhammar <blixtdackhammar@gmail.com>
2021-01-19 12:36:44 +0100

    Changed API to handle profile pictures separately

--- FileAPI/FileUploader/src/main/java/com/bezkoder/spring/files/upload/db/controller/FileController.java
+++ FileAPI/FileUploader/src/main/java/com/bezkoder/spring/files/upload/db/controller/FileController.java
@@ -2,23 +2,18 @@ package com.bezkoder.spring.files.upload.db.controller;
 
 import com.bezkoder.spring.files.upload.db.message.ResponseFile;
 import com.bezkoder.spring.files.upload.db.message.ResponseMessage;
-import com.bezkoder.spring.files.upload.db.model.FileDB;
-import com.bezkoder.spring.files.upload.db.repository.FileDBRepository;
 import com.bezkoder.spring.files.upload.db.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Controller
 /*@RequestMapping("/api")*/
@@ -49,7 +43,7 @@ public class FileController {
       String stringPath = uuid + fileName;
       Files.write(path,bytes);
       message = stringPath;
-      storageService.store(file, stringPath, userId);
+      storageService.storePicture(file, stringPath, userId);
       return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     } catch (Exception e) {
       message = "Could not upload the file: " + file.getOriginalFilename() + "!";
@@ -85,6 +79,42 @@ public class FileController {
     }
     return ResponseEntity.status(HttpStatus.OK).body(filesByUserId);
   }
+
+  @PostMapping("/uploadprofilepic")
+  public ResponseEntity<ResponseMessage> uploadProFilePic(@RequestParam("file") MultipartFile file,@RequestParam("userId") int userId) {
+    String message = "";
+    try {
+      String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+      byte[] bytes = file.getBytes();
+      UUID uuid = UUID.randomUUID();
+      Path path = Paths.get(imgpath + uuid + fileName);
+      String stringPath = uuid + fileName;
+      Files.write(path,bytes);
+      message = stringPath;
+      storageService.storeProfilePic(file, stringPath, userId);
+      return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+    } catch (Exception e) {
+      message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+    }
+  }
+
+  @GetMapping("/profilepics/{userId}")
+  public ResponseEntity<List<ResponseFile>> getProfilePicsByUserId(@PathVariable(value = "userId") int userId) {
+    List<ResponseFile> profilepics = storageService.getAllProfilePics().map(dbFile -> new ResponseFile(
+            dbFile.getName(),
+            dbFile.getPath(),
+            dbFile.getUserId())).collect(Collectors.toList());
+    List<ResponseFile> profilepicsByUserId = new ArrayList<>();
+    for(ResponseFile file : profilepics) {
+      if(file.getUserId() == userId) {
+        profilepicsByUserId.add(file);
+      }
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(profilepicsByUserId);
+  }
+
+
   /*@Transactional
 	@DeleteMapping("/removeimage/{id}")
 	public ResponseEntity<?> deleteImage(@PathVariable String id) {
